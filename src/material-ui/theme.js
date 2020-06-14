625738a02af49679a997589ba55b9be4b015a784
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-06-14 13:54:31 +0200

    added mui palette customization

--- src/material-ui/theme.js
+++ src/material-ui/theme.js
@@ -1,5 +1,8 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import palette from './custom/palette';
 
-const theme = createMuiTheme({});
+const theme = createMuiTheme({
+  palette: palette,
+});
 
 export default theme;
