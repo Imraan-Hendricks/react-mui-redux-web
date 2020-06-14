a2a3ffbc59d199bd826fc3bd92eabfa229989e80
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-06-14 13:46:04 +0200

    setup mui theme provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import theme from './material-ui/theme';
+import { ThemeProvider } from '@material-ui/core/styles';
 import Routes from './routes/routes';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div>
-        <Routes />
-      </div>
+      <ThemeProvider theme={theme}>
+        <div>
+          <Routes />
+        </div>
+      </ThemeProvider>
     </Provider>
   );
 };
