0dd97f6b8fd739b799b64d6e8dbbd78ac2fb454e
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-07-02 00:50:27 +0200

    added persist migrations to root model

--- src/redux/_root-persist.js
+++ src/redux/_root-persist.js
@@ -1,12 +1,31 @@
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import { createMigrate } from 'redux-persist';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+const migrations = {
+  0: (state) => {
+    return {
+      // migration clear out ex1 state
+      ...state,
+      ex1: undefined,
+    };
+  },
+  1: (state) => {
+    return {
+      // migration to keep only ex1 state
+      ex1: state.ex1,
+    };
+  },
+};
+
 const persistConfig = {
   key: 'model',
+  version: -1,
   storage,
   whitelist: ['ex1'],
   stateReconciler: autoMergeLevel2,
+  migrate: createMigrate(migrations, { debug: false }),
 };
 
 const persist = (reducer) => {
