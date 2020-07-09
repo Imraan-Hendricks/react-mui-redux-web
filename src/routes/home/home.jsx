779c34b7a2b1de6d93a9c53ee7067aa3b1d2307f
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-07-10 00:09:55 +0200

    added set active nav to home route

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import navbarOperations from '../../components/navbar/_navbar-operations';
+import { useDispatch } from 'react-redux';
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(navbarOperations.setActive('home'));
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Home</h1>
