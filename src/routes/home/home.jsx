75f9e5868b6adee6d55476109ae08c0d83d04bc3
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-07-13 02:57:00 +0200

    updated home route

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,14 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Auth from '../../components/auth/auth';
-import navbarOperations from '../../components/navbar/redux/navbar-operations';
-import { useDispatch } from 'react-redux';
+import useHome from './use-home';
 
 const Home = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(navbarOperations.setActive('home'));
-  }, [dispatch]);
+  useHome();
 
   return (
     <div>
